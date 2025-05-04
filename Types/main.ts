66163dfd48662058d3361679adcb91110eb43a4b
conorkeane01 <01conorjkeane@gmail.com>
2025-05-04 13:48:17 +0100

    Types

---
+++ Types/main.ts
@@ -0,0 +1,11 @@
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, 'a']; //bad practice avoid
+
+enum Color { Red = 0, Green = 1, BLue = 2};
+let backgroundColor = Color.BLue;
+
